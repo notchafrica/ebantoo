33d1245e474a6584a37888091c81cb7f25275dbc
nzouda777 <nzoudarodrigue@gmail.com>
2022-11-15 01:42:35 +0100

    theme sections

--- pages/home/app.js
+++ pages/home/app.js
@@ -1,16 +1,32 @@
 var swiper = new Swiper(".mySwiper", {
     direction: "vertical",
-    //loop: true,
+    loop: true,
     pagination:{
         el: ".swiper-pagination",
         clickable: true,
         direction: "horizontal"
     },
     autoplay: {
-       // delay: 5000,
+        delay: 5000,
     },
     navigation: {
         nextEl: ".rod-button-next",
         prevEl: ".rod-button-prev"
     }
 });
+
+var swiper = new Swiper(".mySwiper2", {
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".mySwiper3", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 3000
+    }
+  });
