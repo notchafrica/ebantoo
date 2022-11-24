cdd9321acae2a3e01a4024c47e9f49768d584903
nzouda777 <nzoudarodrigue@gmail.com>
2022-11-24 16:19:13 +0100

    maj

--- pages/home/app.js
+++ pages/home/app.js
@@ -1,32 +1,33 @@
-// var swiper = new Swiper(".mySwiper", {
-//     direction: "vertical",
-//     loop: true,
-//     pagination:{
-//         el: ".swiper-pagination",
-//         clickable: true,
-//         direction: "horizontal"
-//     },
-//     autoplay: {
-//         delay: 5000,
-//     },
-//     navigation: {
-//         nextEl: ".rod-button-next",
-//         prevEl: ".rod-button-prev"
-//     }
-// });
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    loop: true,
+    pagination:{
+        el: ".swiper-pagination",
+        clickable: true,
+        direction: "horizontal"
+    },
+    autoplay: {
+        delay: 5000,
+    },
+    navigation: {
+        nextEl: ".rod-button-next",
+        prevEl: ".rod-button-prev"
+    }
+});
 
-// var swiper = new Swiper(".mySwiper2", {
-//     loop: true,
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-//   });
+var swiper = new Swiper(".mySwiper2", {
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
-//   var swiper = new Swiper(".mySwiper3", {
-//     slidesPerView: 3,
-//     spaceBetween: 30,
-//     autoplay: {
-//         delay: 3000
-//     }
-//   });
+  var swiper = new Swiper(".mySwiper3", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 3000
+    }
+  });
