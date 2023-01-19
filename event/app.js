3784877f8d5b36388a032f823bcda9d09dc6a5e3
nzouda777 <nzoudarodrigue@gmail.com>
2023-01-19 12:40:46 +0100

    maj

--- event/app.js
+++ event/app.js
@@ -84,9 +84,13 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  const $ = (element) => {return document.querySelector(element)}
+  // document.getElementById("demo").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
-
+   $(".day").innerHTML = days + "Jr"
+   $(".hours").innerHTML = hours + "h"
+   $(".min").innerHTML = minutes + "min"
+   $(".sec").innerHTML = seconds + "s"
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
